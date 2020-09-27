02f34922e7c18ab1b251b3ca92f2449d3ad825a6
Rodrigo Fassa <rodrigofassa29@gmail.com>
2020-09-27 09:11:55 -0300

    Initial Commit

---
+++ AnTeStuP/Controllers/TeacherController.cs
@@ -0,0 +1,45 @@
+﻿using AnTeStuP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnTeStuP.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ILogger<TeacherController> _logger;
+
+        public TeacherController(ILogger<TeacherController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrEdit()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AnTeStuP/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnTeStuP.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Discipline> Disciplines { get; set; }
+
+    }
+}
